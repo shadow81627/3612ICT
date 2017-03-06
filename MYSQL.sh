fe84105ee873c7a58b6919738a94563517c50413
Damien Robinson <damien.robinson@griffithuni.edu.au>
2017-03-06 23:20:41 +0000

    Added a php file for connecting to the sql server.

--- MYSQL.sh
+++ MYSQL.sh
@@ -9,3 +9,8 @@ mysql-ctl stop
 # run the MySQL interactive shell
 mysql-ctl cli
 
+# Hostname - $IP (The same local IP as the application you run on Cloud9)
+# Port - 3306 (The default MySQL port number)
+# User - $C9_USER (Your Cloud9 user name)
+# Password - "" (No password since you can only access the DB from within the workspace)
+# Database - c9 (The database username)
