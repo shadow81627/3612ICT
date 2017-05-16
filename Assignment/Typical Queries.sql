db3c6aba78d624dd56e8987975626c897fbe3c20
Damien Robinson <damien.robinson@griffithuni.edu.au>
2017-05-16 13:22:21 +1000

    Fixed the syntax errors to make all the queries run

--- Assignment/Typical Queries.sql
+++ Assignment/Typical Queries.sql
@@ -20,7 +20,7 @@ WHERE customer_id = 1;
 
 -- Supplier add a new item
 INSERT INTO item 
-VALUES(null, "pasta", "great original italian pasta", null);
+VALUES(1, "pasta", "great original italian pasta", null);
 
 -- View all items
 SELECT * 
@@ -36,15 +36,15 @@ UPDATE item
 SET item_name = "pizza", item_description = "great original italian pasta", item_image = null 
 WHERE item_id = 1;
 
--- Supplier delete item
+/*-- Supplier delete item
 DELETE FROM item 
-WHERE item_id = 1;
+WHERE item_id = 1; */
 
 -- Supplier
 
 -- Supplier create 
 INSERT INTO supplier 
-VALUES(null, "coles", null, 1234);
+VALUES(1, "coles", null, 1234);
 
 -- View all Suppliers
 SELECT * 
@@ -64,7 +64,7 @@ WHERE supplier_id = 1;
 
 -- Supplier Creates new product
 INSERT INTO product 
-VALUES(null, 1, 1, 3.20, "kg");
+VALUES(1, 1, 1, 3.20, "kg");
 
 -- View all product
 SELECT * 
@@ -85,15 +85,15 @@ UPDATE product
 SET product_price = 4.20, product_unit = "box"
 WHERE product_id = 1;
 
--- Supplier delete product
+/*-- Supplier delete product
 DELETE FROM product
-WHERE product_id = 1;
+WHERE product_id = 1;*/
 
 -- Product Order
 
 -- Customer creates order
 INSERT INTO product_order 
-VALUES( null, 1, 1, 3, "kg", "21 Fake street", null, null, null);
+VALUES( 1, 1, 1, 3, "kg", "21 Fake street", null, null, null);
 
 -- View a particular customers orders
 SELECT * 
@@ -102,64 +102,64 @@ WHERE customer_id = 1;
 
 -- Supplier views thier orders and the customer contact detials for each order
 SELECT * 
-FROM product_order, customer 
-WHERE supplier_id = 1 
+FROM product_order, customer, product
+WHERE supplier_id = 1
 AND product_order.customer_id = customer.customer_id;
 
 -- Customer update order 
 UPDATE product_order 
-SET product_order_quantity = 4.2, product_order_unit = "grams", product_order_address = "22 Fake strret", product_order_due_date = NOW(), NOW()
+SET product_order_quantity = 4.2, product_order_unit = "grams", product_order_address = "22 Fake strret", product_order_date_due = null
 WHERE product_order_id = 1;
 
--- Customer cancel order
+/*-- Customer cancel order
 DELETE FROM product_order 
-WHERE product_order_id = 1;
+WHERE product_order_id = 1;*/
 
 -- Inventory
+
+-- Supplier add a new inventory
+INSERT INTO inventory 
+VALUES(1, 1, "fridge", "the cold room out back", "23 Fake street");
 
 -- View all inventories for a particular supplier
 SELECT * 
 FROM inventory
 WHERE supplier_id = 1;
 
--- Supplier add a new inventory
-INSERT INTO inventory 
-VALUES(null, "fridge", "the cold room out back", "23 Fake street");
-
 -- Supplier update an inventory
 UPDATE inventory 
 SET inventory_name = "pantry", inventory_description = "The cupboard in the kitchen", inventory_address = "21 Fake street" 
 WHERE inventory_id = 1;
 
--- Supplier delete inventory
+/*-- Supplier delete inventory
 DELETE FROM inventory 
-WHERE inventory_id = 1;
+WHERE inventory_id = 1; */
 
 -- Stock
 
 -- Supplier add a new stock
 INSERT INTO stock 
-VALUES(null, 1, 1, 3, "kg", null);
+VALUES(1, 1, 1, 3, "kg", null);
 
 -- View all stock for a particular inventory
 SELECT * 
 FROM stock
-WHERE invenetory_id = 1;
+WHERE inventory_id = 1;
 
 -- Supplier update the quantity of a stocked item
 UPDATE stock 
-SET stock_quantity = 7, stock_unit = "gram"
+SET stock_quantity = 7, stock_unit = "grams"
 WHERE stock_id = 1;
 
--- Supplier delete stock
+/*-- Supplier delete stock
 DELETE FROM stock 
-WHERE stock_id = 1;
+WHERE stock_id = 1; */
 
 -- Recipe
 
 -- Supplier add a new recipe
 INSERT INTO recipe 
-VALUES(null, 1, 3, "kg");
+VALUES(1, 1, 3, "kg");
 
 -- View all recipes
 SELECT * 
@@ -170,28 +170,28 @@ UPDATE recipe
 SET recipe_quantity = 4, recipe_unit = "grams" 
 WHERE recipe_id = 1;
 
--- Supplier delete recipe
+/*-- Supplier delete recipe
 DELETE FROM recipe 
-WHERE recipe_id = 1;
+WHERE recipe_id = 1; */
 
 -- Process 
+
+-- Supplier add a new process
+INSERT INTO process 
+VALUES(1, 1, 1, "Add pasta", "Add pasta to the boiling water", null);
 
 -- View all processes
 SELECT * 
 FROM process;
-
--- Supplier add a new process
-INSERT INTO process 
-VALUES(null, 1, 1, "Add pasta", "Add pasta to the boiling water", null);
 
 -- Supplier Update an process
 UPDATE process 
 SET recipe_id = 1, process_precedence = 2, process_name = "Add pizza", process_description = "pour pizza onto your plate", process_image = null 
 WHERE process_id = 1;
 
--- Supplier delete process
+/*-- Supplier delete process
 DELETE FROM process 
-WHERE process_id = 1;
+WHERE process_id = 1; */
 
 -- Ingredient 
 
@@ -206,8 +206,8 @@ FROM ingredient;
 -- Supplier Update an ingredient
 UPDATE ingredient 
 SET ingredient_quantity = 4, ingredient_unit = "grams" 
-WHERE ingredient_id = 1;
+WHERE item_id = 1 AND process_id = 1;
 
--- Supplier delete ingredient
+/*-- Supplier delete ingredient
 DELETE FROM ingredient 
-WHERE ingredient_id = 1;
+WHERE ingredient_id = 1; */
