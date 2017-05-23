717b83ace6e9ef6bf44857aabb0db8e479c3b913
Damien Robinson <damien.robinson@griffithuni.edu.au>
2017-05-23 15:24:31 +1000

    Created a sql file that is used to insert test values into the database

---
+++ Assignment/Insert.sql
@@ -0,0 +1,29 @@
+-- Insert Suppliers
+INSERT INTO supplier 
+VALUES(1, "coles", null, 1234);
+
+INSERT INTO supplier 
+VALUES(2, "woolies", null, 12345);
+
+INSERT INTO supplier 
+VALUES(3, "aldi", null, 12346);
+
+-- Insert Items
+INSERT INTO item 
+VALUES(1, "pasta", "great original italian pasta", null);
+
+INSERT INTO item 
+VALUES(2, "pizza", "great original italian pizza", null);
+
+INSERT INTO item 
+VALUES(3, "cheese", "delicous parmasain cheese", null);
+
+-- Insert Products
+INSERT INTO product 
+VALUES(1, 1, 1, 3.20, "kg");
+
+INSERT INTO product 
+VALUES(2, 2, 2, 4.20, "kg");
+
+INSERT INTO product 
+VALUES(3, 3, 3, 5.20, "kg");
