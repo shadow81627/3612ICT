458ca94080ab298023c7e32fd02b635fb7f82f00
Damien Robinson <damien.robinson@griffithuni.edu.au>
2017-05-25 09:05:28 +1000

    Added the rest of the insert statements

--- Assignment/Insert.sql
+++ Assignment/Insert.sql
@@ -27,3 +27,73 @@ VALUES(2, 2, 2, 4.20, "kg");
 
 INSERT INTO product 
 VALUES(3, 3, 3, 5.20, "kg");
+
+-- Insert Customer
+INSERT INTO customer 
+VALUES(1, "Ben", "ben@gmail.com", "21 Fake street");
+
+INSERT INTO customer 
+VALUES(2, "Harry", "harry@gmail.com", "22 Fake street");
+
+INSERT INTO customer 
+VALUES(3, "Bruce", "bruce@gmail.com", "23 Fake street");
+
+-- Insert Product order
+INSERT INTO product_order 
+VALUES( 1, 1, 1, 3.20, "kg", "21 Fake street", null, null, null);
+
+INSERT INTO product_order 
+VALUES( 1, 2, 2, 5.7, "kg", "22 Fake street", null, null, null);
+
+INSERT INTO product_order 
+VALUES( 1, 3, 3, 20.5, "kg", "23 Fake street", null, null, null);
+
+-- Insert inventory
+INSERT INTO inventory 
+VALUES(1, 1, "fridge", "the cold room out back", "21 Fake street");
+
+INSERT INTO inventory 
+VALUES(2, 2, "pantry", "cupboard in the kitchen back", "22 Fake street");
+
+INSERT INTO inventory 
+VALUES(3, 3, "shed", "shed out back", "23 Fake street");
+
+-- Insert stock
+INSERT INTO stock 
+VALUES(1, 1, 1, 3, "kg", null);
+
+INSERT INTO stock 
+VALUES(1, 2, 2, 7.8, "kg", null);
+
+INSERT INTO stock 
+VALUES(1, 3, 3, 52.4, "kg", null);
+
+-- Insert recipies
+INSERT INTO recipe 
+VALUES(1, 1, 3.2, "kg");
+
+INSERT INTO recipe 
+VALUES(2, 2, 4.3, "kg");
+
+INSERT INTO recipe 
+VALUES(3, 3, 9.3, "kg");
+
+-- Insert process
+INSERT INTO process 
+VALUES(1, 1, 1, "Add pasta", "Add pasta to the boiling water", null);
+
+INSERT INTO process 
+VALUES(2, 2, 1, "Add pizza", "Add pizza to the boiling water", null);
+
+INSERT INTO process 
+VALUES(3, 3, 1, "Add cheese", "Add cheese to the boiling water", null);
+
+-- Insert ingrediets
+INSERT INTO ingredient 
+VALUES(1, 1, 3, "kg");
+
+INSERT INTO ingredient 
+VALUES(2, 1, 3, "kg");
+
+INSERT INTO ingredient 
+VALUES(3, 1, 3, "kg");
