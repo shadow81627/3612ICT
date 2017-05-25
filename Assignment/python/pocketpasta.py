285d4e12d6091deffd0ea1a689f8df5923e3866a
Damien Robinson <damien.robinson@griffithuni.edu.au>
2017-05-25 15:08:26 +1000

    added product order

--- Assignment/python/pocketpasta.py
+++ Assignment/python/pocketpasta.py
@@ -30,43 +30,39 @@ class Login(object):
 # Supplier 
 @cherrypy.popargs('supplier_id')
 class Supplier(object):
-    def _init_(self):
-        self.products = Product()
+	def _init_(self):
+		self.product = Product()
         #self.inventories = Inventory()
         #self.orders = Order()
 
-    @cherrypy.expose
-    def index(self, supplier_id = None):
-        # Supplier list
-        if supplier_id == None:
+	@cherrypy.expose
+	def index(self, supplier_id = None):
+		# Supplier list
+		if supplier_id == None:
 			header = DB.table_header('supplier')
 			data = DB.get_all_supplier()
 			return DB.print_table(header, data)
 
-        # Supplier detail
-        else:
+		# Supplier detail
+		else:
 			header = DB.table_header('supplier')
 			data = DB.get_supplier_detail(supplier_id)
 			return DB.print_table(header, data)
 
-    @cherrypy.expose
-    def product(self, supplier_id = None):
-        if supplier_id != None:
+	@cherrypy.expose
+	def product(self, supplier_id = None):
+		if supplier_id != None:
+			header = DB.table_header('supplier_items')
 			data = DB.get_supplier_product(supplier_id)
-			result = ""
-			for row in data:
-				for attribute in row:
-					result += str(attribute) + " "
-				result += "<br>"
-			return result
-			#return "Error: unable to fecth data"
+			return DB.print_table(header, data)
 			
 	@cherrypy.expose
-	def delete(self, supplier_id):
-		insert = DB.delete_supplier(supplier_id)
-		header = DB.table_header('supplier')
-		data = DB.get_all_supplier()
-		return DB.print_table(header, data)
+	def delete(self, supplier_id = None):
+		if supplier_id != None:
+			insert = DB.delete_supplier(supplier_id)
+			header = DB.table_header('supplier')
+			data = DB.get_all_supplier()
+			return DB.print_table(header, data)
 		
 
 @cherrypy.popargs('product_id')
@@ -74,16 +70,18 @@ class Product(object):
     @cherrypy.expose
     def index(self, supplier_id = None, product_id = None):
 		if product_id == None:
+			header = DB.table_header('product')
 			data = DB.get_all_product()
-			result = ""
-			for row in data:
-				for attribute in row:
-					result += str(attribute) + " "
-				result += "<br>"
-			return result
-			#return "Error: unable to fecth data"
+			return DB.print_table(header, data)
 		else:
 			return 'Product id %s' %(product_id)
+			
+class Order(object):
+	@cherrypy.expose
+	def index(self):
+		header = DB.table_header('product_order')
+		data = DB.get_all_product_order()
+		return DB.print_table(header, data)
 			
 class Create(object):
 	@cherrypy.expose
@@ -95,6 +93,13 @@ class Create(object):
 		insert = DB.insert_supplier(supplier_id, supplier_name, supplier_image, supplier_ABN)
 		header = DB.table_header('supplier')
 		data = DB.get_all_supplier()
+		return DB.print_table(header, data)
+	
+	@cherrypy.expose
+	def order(self, product_order_id = None, product_id = None, customer_id = None, quantity = None, unit = None, address = None, due_date = None, date_created = None, date_delivered = None):
+		insert = DB.insert_product_order(product_order_id, product_id, customer_id, quantity, unit, address, due_date, date_created, date_delivered)
+		header = DB.table_header('product_order')
+		data = DB.get_all_product_order()
 		return DB.print_table(header, data)
 		
 class Delete(object):
@@ -114,6 +119,7 @@ if __name__ == '__main__':
 	cherrypy.tree.mount(Login(), '/')
 	cherrypy.tree.mount(Supplier(), '/supplier')
 	cherrypy.tree.mount(Product(), '/product')
+	cherrypy.tree.mount(Order(), '/order')
 	cherrypy.tree.mount(Create(), '/create') 
 	cherrypy.tree.mount(Delete(), '/delete')
 
